38b4815bec7de4628617c8d9627cdf75a0bb861c
Gabriel de Freitas <contato.gabrieldefreitas@gmail.com>
2020-06-25 18:48:20 -0300

    FinalVersion2

--- tests/testSchool/TestSchoolGrades.java
+++ tests/testSchool/TestSchoolGrades.java
@@ -7,7 +7,13 @@ import org.junit.jupiter.api.Test;
 
 import school.Discipline;
 import school.SchoolGrades;
+import school.Student;
+import systemExceptions.DisciplineIsEmptyException;
 import systemExceptions.DisciplineNotFoundException;
+import systemExceptions.DuplicatedCodeException;
+import systemExceptions.InvalidCodeException;
+import systemExceptions.InvalidGradeException;
+import systemExceptions.InvalidStudentParametersException;
 
 class TestSchoolGrades {
 	private static SchoolGrades sG = new SchoolGrades();
@@ -20,7 +26,7 @@ class TestSchoolGrades {
 		});
 
 	}
-	
+
 	@Test
 	void testHasDiscipline() {
 		Discipline d = new Discipline("Lab2", 1);
@@ -29,14 +35,14 @@ class TestSchoolGrades {
 		sG.addDiscipline(d2);
 		assertEquals(1, sG.getNumOfDisciplines());
 	}
-	
+
 	@Test
 	void testRemoveDisciplineFalse() {
 		Assertions.assertThrows(DisciplineNotFoundException.class, () -> {
 			sG.removeDiscipline(10);
 		});
 	}
-	
+
 	@Test
 	void testRemoveDisciplineTrue() {
 		try {
@@ -46,7 +52,24 @@ class TestSchoolGrades {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
+	@Test
+	void testGetAverageOfAllDisciplines() {
+		sG.addDiscipline((new Discipline("Prog2", 99)));
+		Student std;
+		try {
+			std = new Student("Gabriel", 99999);
+			std.setGradeA(10);
+			std.setGradeB(10);
+			sG.addStudentToDiscipline(std, 99);
+
+			assertEquals(10, sG.getAverageOfAllDisciplines());
+		} catch (InvalidStudentParametersException | InvalidCodeException | DisciplineNotFoundException
+				| DuplicatedCodeException | InvalidGradeException | DisciplineIsEmptyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 }
